df79e2d1ca4c0ceae00cde25491f469c6268656c
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-12-06 09:07:37 +0100

    Wygląd + wypisywanie daty w milisekundach

--- app/src/main/java/com/example/wyjazd/MainActivity.kt
+++ app/src/main/java/com/example/wyjazd/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.wyjazd
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.core.view.get
+import kotlin.time.Duration.Companion.days
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val data_przyjazdu = findViewById<Button>(R.id.start)
+        val data_wyjazdu = findViewById<Button>(R.id.stop)
+        val text = findViewById<TextView>(R.id.dane)
+        val kalendarz = findViewById<CalendarView>(R.id.selektor)
+        kalendarz.setMinDate(kalendarz.date)
+
+        data_przyjazdu.setOnClickListener {
+            var data_przyj = kalendarz.getDate()
+
+
+        }
+
+
     }
 }
