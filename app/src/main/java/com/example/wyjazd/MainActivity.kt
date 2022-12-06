bb2ce428dcc032dbf51421c06db393d4007cbea6
Palczyk69 <lukasz.holowacz5@gmail.com>
2022-12-06 11:03:44 +0100

    wypisywanie daty

--- app/src/main/java/com/example/wyjazd/MainActivity.kt
+++ app/src/main/java/com/example/wyjazd/MainActivity.kt
@@ -7,7 +7,9 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.core.view.get
+import java.text.SimpleDateFormat
 import kotlin.time.Duration.Companion.days
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +24,17 @@ class MainActivity : AppCompatActivity() {
 
         data_przyjazdu.setOnClickListener {
             var data_przyj = kalendarz.getDate()
-
-
+            var milisekundy = data_przyj
+                val date = milisekundy
+                val formattedDateAsDigitMonth = SimpleDateFormat("dd/MM/yyyy")
+                text.text = "Data przyjazdu" + formattedDateAsDigitMonth.format(date) +"\n"
         }
-
-
+        data_wyjazdu.setOnClickListener {
+            var data_wyj = kalendarz.getDate()
+            var milisekundy = data_wyj
+            val date = milisekundy
+            val formattedDateAsDigitMonth = SimpleDateFormat("dd/MM/yyyy")
+            text.text = text.text.toString() +  "Data wyjazdu" + formattedDateAsDigitMonth.format(date) +"\n"
+        }
     }
 }
